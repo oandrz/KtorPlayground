00ab469a1565dfa2ae5ba18f0d7cdecd973bcd62
andreas.oentoro <andreas.oentoro@grabtaxi.com>
2023-11-26 19:21:06 +0800

    Add Connection Pooling

--- src/main/kotlin/com/dre/Application.kt
+++ src/main/kotlin/com/dre/Application.kt
@@ -3,13 +3,17 @@ package com.dre
 import com.dre.database.DatabaseFactory
 import com.dre.plugins.*
 import io.ktor.server.application.*
-
+/*
+* TO build jar for beanstalk
+* ./gradlew :buildFatJar then look at /build/libs
+* */
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
+
 fun Application.module() {
-    DatabaseFactory.init()
+    DatabaseFactory.init(environment.config)
     configureSession()
     configureAuthentication()
     configureSerialization()
--- src/main/kotlin/com/dre/database/DatabaseFactory.kt
+++ src/main/kotlin/com/dre/database/DatabaseFactory.kt
@@ -1,17 +1,33 @@
 package com.dre.database
 
 import com.dre.model.Users
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.ktor.http.ContentDisposition.Companion.File
+import io.ktor.server.config.*
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.io.File
 
 object DatabaseFactory {
-    fun init() {
-        val driverClassName = "org.h2.Driver"
-        val jdbcURL = "jdbc:h2:file:./build/db"
-        val database = Database.connect(jdbcURL, driverClassName)
+    fun init(config: ApplicationConfig) {
+        val driverClassName = config.property("storage.class_name").getString()
+        val jdbcURL = config.property("storage.jbdc_url").getString() +
+                (config.propertyOrNull("storage.dbFilePath")?.getString()?.let {
+                    File(it).canonicalFile.absolutePath
+                } ?: "")
+        // Create database without connection pooling
+//        val database = Database.connect(jdbcURL, driverClassName)
+        // Create database with connection pooling
+        val database = Database.connect(
+            createHikariDataSource(
+                url = jdbcURL,
+                driver = driverClassName
+            )
+        )
         /*
         * If you have only one database, you can omit it. In this case, Exposed automatically uses the last connected database for transactions.
         * */
@@ -23,4 +39,23 @@ object DatabaseFactory {
 
     suspend fun <T> dbQuery(block: suspend () -> T): T =
         newSuspendedTransaction(Dispatchers.IO) { block() }
+
+    private fun createHikariDataSource(
+        url: String,
+        driver: String
+    ) = HikariDataSource(HikariConfig().apply {
+        driverClassName = driver
+        jdbcUrl = url
+        maximumPoolSize = 3 // size of the connection pool can reach
+        /*
+        * When isAutoCommit is set to true, each statement is committed immediately after execution.
+        * When set to false, the developer needs to explicitly commit the transaction using the commit() method after
+        * executing a series of statements. This setting can affect the atomicity of transactions, where a series of
+        * statements can be treated as a single unit
+        * */
+        isAutoCommit = false
+        // transactionIsolation behavior of transactions concerning concurrent access and changes made by other transactions
+        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        validate()
+    })
 }
